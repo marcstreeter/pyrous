457e765ef3ae697406d169843306a903c16fbc69
marcstreeter <mstreeter@gmail.com>
2016-03-24 12:38:04 -0500

    update version and uncommented other fields

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(name="rous",
-      version="0.1.2",
-      #description="a package for rodents of unusual size",
-      #url="https://github.com/marcstreeter/pygroup",
-      #author="Marc Streeter",
-      #author_email="mstreeter@gmail.com",
-      #license="Hello, my name is Inigo Montoya, you killed my father prepare to die",
+      version="0.2",
+      description="a package for rodents of unusual size",
+      url="https://github.com/marcstreeter/pygroup",
+      author="Marc Streeter",
+      author_email="mstreeter@gmail.com",
+      license="Hello, my name is Inigo Montoya, you killed my father prepare to die",
       packages=["rous"],
       install_requires=["requests"],
       zip_safe=False)
