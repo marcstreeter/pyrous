ccad5ca17c4f167a11da6b4f0deb437075a9418d
marcstreeter <mstreeter@gmail.com>
2016-03-24 16:07:54 -0500

    fini

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(name="rous",
-      version="0.2",
+      version="0.3",
       description="a package for rodents of unusual size",
       url="https://github.com/marcstreeter/pygroup",
       author="Marc Streeter",
       author_email="mstreeter@gmail.com",
-      license="Hello, my name is Inigo Montoya, you killed my father prepare to die",
+      license="you killed my father prepare to die",
       packages=["rous"],
-      install_requires=["requests"],
+      install_requires=["requests", "beautifulsoup4"],
       zip_safe=False)
