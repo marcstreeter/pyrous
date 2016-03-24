79429fd155ff2e0e411d6d78944e1e480dbaece5
marcstreeter <mstreeter@gmail.com>
2016-03-24 08:41:36 -0500

    removed unneeded

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(name="rous",
       version="0.1",
-      description="a package for rodents of unusual size",
-      url="https://github.com/marcstreeter/pygroup",
-      author="Marc Streeter",
-      author_email="mstreeter@gmail.com",
-      license="Hello, my name is Inigo Montoya, you killed my father prepare to die",
+      #description="a package for rodents of unusual size",
+      #url="https://github.com/marcstreeter/pygroup",
+      #author="Marc Streeter",
+      #author_email="mstreeter@gmail.com",
+      #license="Hello, my name is Inigo Montoya, you killed my father prepare to die",
       packages=["rous"],
       install_requires=["requests"],
       zip_safe=False)
